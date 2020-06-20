a5b1b88095108cf91a2c806803c8370af9768c86
Jenny <jennyqr10110@gmail.com>
2020-06-20 18:39:52 +0200

    Neue Mathe Klasse

--- src/MathTest.java
+++ src/MathTest.java
@@ -1,7 +1,11 @@
 public class MathTest {
     public static void main (String [] args){
         MyMath m1 = new MyMath();
-        int [] tset = {2,6,67,3};
-        System.out.println(m1.arrSumRechnen(tset));
+        int [] test = {2,-5,67,3};
+        System.out.println(m1.arrSumRechnen(test));
+        System.out.println(m1.arrPositivInteger(test));
+        System.out.println(m1.arrNegativInteger(test));
+        System.out.println(m1.arrGeradeInteger(test));
+        System.out.println(m1.arrUngeradeInteger(test));
     }
 }
--- src/MyMath.java
+++ src/MyMath.java
@@ -6,4 +6,36 @@ public class MyMath {
         }
         return(sum);
     }
+    public int arrPositivInteger (int [] intgerArr){
+        int possitivSum = 0;
+        for (int i = 0;i<intgerArr.length;i++){
+            if(intgerArr[i]>0) {possitivSum++;}
+        }
+        return(possitivSum);
+    }
+    public int arrNegativInteger (int [] intgerArr){
+        int negativSum = 0;
+        for (int i = 0;i<intgerArr.length;i++){
+            if(intgerArr[i]<0) {negativSum++;}
+        }
+        return(negativSum);
+    }
+    public int arrUngeradeInteger (int [] intgerArr) {
+        int ungeradeSum = 0;
+        for (int i = 0; i < intgerArr.length; i++) {
+            if (intgerArr[i] % 2 != 0) {
+                ungeradeSum++;
+            }
+        }
+        return (ungeradeSum);
+    }
+    public int arrGeradeInteger (int [] intgerArr) {
+        int geradeSum = 0;
+        for (int i = 0; i < intgerArr.length; i++) {
+            if (intgerArr[i] % 2 == 0) {
+                geradeSum++;
+            }
+        }
+        return (geradeSum);
+    }
 }
